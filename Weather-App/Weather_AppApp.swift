49a50076d3a16bfb656c1c4432e40c61613040bf
himanshugoswamii <goswamihimanshu.8826@gmail.com>
2026-07-28 16:39:08 -0500

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,21 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Himanshu Goswami on 7/28/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Weather_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
